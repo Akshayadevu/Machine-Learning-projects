7dee7931a8d6958ee9f0bb6f85d7e97b2be412d5
Devu Akshaya <akshayadevuakshaya@gmail.com>
2025-07-02 14:37:20 +0530

    Add files via upload

---
+++ rock vs mine/rock vs mine.py
@@ -0,0 +1,5 @@
+import numpy as np #creating numpy arrays
+import pandas as pd #loading our data into tables known as dataframes
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
